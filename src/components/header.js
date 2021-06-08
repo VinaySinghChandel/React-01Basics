b504e5f240033184bd6e05955c98efbdb8e07fb0
Vinay Singh Chandel <vinaysinghchandel27@gmail.com>
2021-06-09 00:32:36 +0530

    done

--- src/components/header.js
+++ src/components/header.js
@@ -11,19 +11,64 @@ import React, { Component } from 'react';
 // }
 
 class Header extends Component {
+    state = {
+        name    :   'Example',
+        email   :   'example@gmail.com',
+        count   :   0
+    }
+   
+    onClickDisplay = (event) => {
+        //console.log('I was changed');
+        this.setState((state, props) => ({
+            count   :   state.count + 1
+        })
+            // count   :   this.state.count + 1
+        );
+    }
+
+    onChangeName = (event) => {
+        this.setState({
+            name    :   event.target.value
+        });
+    }
+
+    onChangeEmail = (event) => {
+        this.setState({
+            email    :   event.target.value
+        });
+    }
     render() {
+       console.log(this.state);
         return(
             <div className="main">
                 <p className="header">Hello Header</p>
                 <table>
                     <colgroup>
-                        <col width="50"/>
-                        <col width="50"/>
+                        <col width="50%"/>
+                        <col width="50%"/>
                     </colgroup>
-                    <tr>
-                        <td><b>Name</b></td>
-                        <td><input type="text"/></td>
-                    </tr>
+                    <tbody>
+                        <tr>
+                            <td><b>Name</b></td>
+                            <td><input type="text" onChange={this.onChangeName}/></td>
+                        </tr>
+                        <tr>
+                            <td><b>Email</b></td>
+                            <td><input type="text" onChange={this.onChangeEmail}/></td>
+                        </tr>
+                        <tr>
+                            <td><button onClick={this.onClickDisplay}>ADD</button></td>
+                        </tr>
+                        <tr>
+                            <td className="result">{this.state.count}. </td>
+                        </tr>
+                        <tr>
+                            <td className="result">{this.state.name}</td>
+                        </tr>
+                        <tr>
+                            <td className="result">{this.state.email}</td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         )
